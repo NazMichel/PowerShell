86be5c816e5f4300df79dfc4d3bcf9bb052ee1c4
Naz Michel <NE.Michel@outlook.com>
2023-09-11 15:59:36 -0700

    new file:   DisableUserAccountLogon30OrMoreDays.ps1

---
+++ DisableUserAccountLogon30OrMoreDays.ps1
@@ -0,0 +1,5 @@
+﻿#Disable user accounts that have not been used to logon with in 30 or more days
+
+Get-ADUser -Filter {Enabled -eq $true} -SearchBase $OU -Properties Name, SamAccountName, LastLogonDate | 
+    Where {($_.LastLogonDate -lt (Get-Date).AddDays(-30)) -and ($_.LastLogonDate -ne $NULL)} | 
+        Sort | Select Name, SamAccountName, LastLogonDate
