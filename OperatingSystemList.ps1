67f62c6edae5112b45382048b04e438e017d4038
Naz Michel <NE.Michel@outlook.com>
2023-09-13 15:44:11 -0700

    09132023 1544

---
+++ OperatingSystemList.ps1
@@ -0,0 +1,7 @@
+﻿#Create list of computers with a particular operating system installed
+
+$CompName = (Get-ADComputer -Filter *).Name
+
+Get-ADComputer -Filter ('Name -like "{0}"' -f $CompName) -Properties OperatingSystem
+
+#Get-ADComputer -Filter 'OperatingSystem -like "*"' -Properties OperatingSystem | Sort OperatingSystem, Name | Format-Table Name, OperatingSystem -AutoSize
