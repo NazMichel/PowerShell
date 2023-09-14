82c5568d0c5eaa22461ab3d2664caeb3c9c9c246
Naz Michel <NE.Michel@outlook.com>
2023-09-14 11:45:16 -0700

    20230914 1445

---
+++ DiskSpaceProject.ps1
@@ -0,0 +1,12 @@
+﻿
+#$LocalDriveC = Get-PSDrive -Name C
+
+#Get List of computers in a table
+$Computers = "LON-CL1","LON-DC1","LON-SVR1"
+
+Foreach($Computer in $Computers){
+
+    
+    Get-CimInstance Win32_LogicalDisk -ComputerName $computer | Select-Object PSComputerName,DeviceID,FreeSpace | Where-Object -Property FreeSpace -NE -Value $NULL
+    #Write-Host "$Computer" -ForegroundColor Yellow
+    }
