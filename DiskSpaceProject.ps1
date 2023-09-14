1ec39251d9cbbe1c6f5ee284072bf883b9401f36
Naz Michel <NE.Michel@outlook.com>
2023-09-14 14:07:00 -0700

    20230914 1707

--- DiskSpaceProject.ps1
+++ DiskSpaceProject.ps1
@@ -2,6 +2,7 @@
 
 $computer = Get-ADComputer -Filter * | Select-Object Name
 
-Get-CimInstance Win32_LogicalDisk -ComputerName $computer.name -Filter DriveType=3 | Select-Object PSComputerName,DeviceID,
-    @{'Name'='Free Space (GB)'; 'Expression'={[string]::Format('{0:N0}',[math]::truncate($_.freespace / 1GB))}},
-    @{'Name'='% Free'; 'Expression'={[string]::Format('{0:N2}',[math]::truncate($_.freespace) / [math]::truncate($_.size) * 100)}}
+Get-CimInstance Win32_LogicalDisk -ComputerName $computer.name -Filter DriveType=3 |
+    Sort-Object PSComputerName | Select-Object PSComputerName,DeviceID,
+        @{'Name'='Free Space (GB)'; 'Expression'={[string]::Format('{0:N0}',[math]::truncate($_.freespace / 1GB))}},
+        @{'Name'='% Free'; 'Expression'={[string]::Format('{0:N2}',[math]::truncate($_.freespace) / [math]::truncate($_.size) * 100)}}
